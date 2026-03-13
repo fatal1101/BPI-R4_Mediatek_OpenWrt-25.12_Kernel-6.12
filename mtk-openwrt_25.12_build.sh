47fe72683487ea6f802272a71a5336c5a6b3a36d
Андрей <156803833+fatal1101@users.noreply.github.com>
2026-03-13 09:07:37 +0300

    Update mtk-openwrt_25.12_build.sh

--- mtk-openwrt_25.12_build.sh
+++ mtk-openwrt_25.12_build.sh
@@ -9,12 +9,12 @@ set -euo pipefail
 readonly OPENWRT_REPO="https://git.openwrt.org/openwrt/openwrt.git"
 #readonly OPENWRT_REPO="/home/user/repo/openwrt"
 readonly OPENWRT_BRANCH="openwrt-25.12"
-readonly OPENWRT_COMMIT="" 
+readonly OPENWRT_COMMIT="c2fe6ca16d0e6c9ec31da709d44263efdf12a3c1" 
 
 readonly MTK_FEEDS_REPO="https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds"
 #readonly MTK_FEEDS_REPO="/home/user/repo/mtk-openwrt-feeds"
 readonly MTK_FEEDS_BRANCH="master"
-readonly MTK_FEEDS_COMMIT="" 
+readonly MTK_FEEDS_COMMIT="b0fefe65a28d5a5b938c9c197d6bbe729484ffef" 
 
 # --- Directory Configuration ---
 readonly SOURCE_DEFAULT_CONFIG_DIR="config"
